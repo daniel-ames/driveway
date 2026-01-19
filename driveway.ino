1ae00c805716cc471dd621ca1b5bd1f829a5a146
dan <daniel.ames@hp.com>
2026-01-19 13:33:40 -0600

    Clean up web behavior. Make web only able to PULSE, and only when the switch is OFF. Show how much longer it will be on in web

--- driveway.ino
+++ driveway.ino
@@ -51,7 +51,8 @@ ESP8266HTTPUpdateServer httpUpdater;
 char httpStr[256] = {0};
 #define SYS_STATUS_PAGE_STR_LEN 2048
 char systemStatusPageStr[SYS_STATUS_PAGE_STR_LEN];
-char responseStr[64];
+char responseStr[128];
+char time_left[64];
 bool lights_on = false;
 bool remote_control_inited = false;
 bool house_switch_on = false;
@@ -70,27 +71,117 @@ bool debounce_started = false;
 unsigned long start_time = 0,
               current_time = 0;
 
+// Converts milliseconds into natural language
+void millisToDaysHoursMinutes(unsigned long milliseconds, char* str, int length)
+{  
+  uint seconds = milliseconds / 1000;
+  memset(str, 0, length);
+
+  if (seconds <= 60) {
+    // It's only been a few seconds
+    // Longest string example, 11 chars: 59 seconds\0
+    snprintf(str, 11, "%d second%s", seconds, seconds == 1 ? "" : "s");
+    return;
+  }
+  uint minutes = seconds / 60;
+  if (minutes <= 60) {
+    // It's only been a few minutes
+    // Longest string example, 11 chars: 59 minutes\0
+    snprintf(str, 11, "%d minute%s", minutes, minutes == 1 ? "" : "s");
+    return;
+  }
+  uint hours = minutes / 60;
+  minutes -= hours * 60;
+  if (hours <= 24) {
+    // It's only been a few hours
+    if (minutes == 0)
+      // Longest string example, 9 chars: 23 hours\0
+      snprintf(str, 9, "%d hour%s", hours, hours == 1 ? "" : "s");
+    else
+      // Longest string example, 24 chars: 23 hours and 59 minutes\0
+      snprintf(str, 24, "%d hour%s and %d minute%s", hours, hours == 1 ? "" : "s", minutes, minutes == 1 ? "" : "s");
+    return;
+  }
+
+  // It's been more than a day
+  uint days = hours / 24;
+  hours -= days * 24;
+  if (minutes == 0)
+    // Longest string example, 23 chars: 9999 days and 23 hours\0
+    snprintf(str, 23, "%d day%s and %d hour%s", days, days == 1 ? "" : "s", hours, hours == 1 ? "" : "s");
+  else
+    // Longest string example, 35 chars: 9999 days, 23 hours and 59 minutes\0
+    snprintf(str, 35, "%d day%s, %d hour%s and %d minute%s", days, days == 1 ? "" : "s", hours, hours == 1 ? "" : "s", minutes, minutes == 1 ? "" : "s");
+}
+
+
 char* getSystemStatus()
 {
   String html;
-  if (house_switch_on) {
-    html = "<!DOCTYPE html><html><head><title>Driveway Lights</title></head><body><span style=\"font-size:90px\">Remote control <span style=\"color:Red;\">disabled</span> because the main switch is on.</span></body></html>";
-    memset(systemStatusPageStr, 0, SYS_STATUS_PAGE_STR_LEN);
-    html.toCharArray(systemStatusPageStr, html.length() + 1);
-    return systemStatusPageStr;
-  }
+  // if (house_switch_on) {
+  //   html = "<!DOCTYPE html><html><head><title>Driveway Lights</title></head><body><span style=\"font-size:90px\">Remote control <span style=\"color:Red;\">disabled</span> because the main switch is on.</span></body></html>";
+  //   memset(systemStatusPageStr, 0, SYS_STATUS_PAGE_STR_LEN);
+  //   html.toCharArray(systemStatusPageStr, html.length() + 1);
+  //   return systemStatusPageStr;
+  // }
   // Pardon the html mess. Gotta tell the browser to not make the text super tiny.
   html = "<!DOCTYPE html><html><head><title>Driveway Lights</title></head><body><p style=\"font-size:36px\">";
-  html += "<script>function toggle() {var xhttp = new XMLHttpRequest();xhttp.open('POST', 'toggle_lights', true);xhttp.onload = function(){console.log(this.responseText);var inner;var style;var val;var disabled = false;if(this.responseText.startsWith('on')) {inner = 'ON';style = 'color:Green;';val = 'Turn Off';} else {inner = 'OFF';style = 'color:Red;';val = 'Turn On';}if(this.responseText.endsWith('on')){val = 'disabled';disabled = true;}document.getElementById('toggle_button').value = val;document.getElementById('toggle_button').disabled = disabled;document.getElementById('lights_span').innerHTML = inner;document.getElementById('lights_span').style = style;};xhttp.send('poop');}</script>";
+  //html += "<script>function pulse() {var xhttp = new XMLHttpRequest();xhttp.open('POST', 'pulse_lights', true);xhttp.onload = function(){console.log(this.responseText);var inner;var style;var val;var disabled = false;if(this.responseText.startsWith('on')) {inner = 'ON';style = 'color:Green;';val = 'Turn Off';} else {inner = 'OFF';style = 'color:Red;';val = 'Turn On';}if(this.responseText.endsWith('on')){val = 'disabled';disabled = true;}document.getElementById('pulse_button').value = val;document.getElementById('pulse_button').disabled = disabled;document.getElementById('lights_span').innerHTML = inner;document.getElementById('lights_span').style = style;};xhttp.send('poop');}</script>";
+  html += 
+    "<script>"
+    "  function pulse() {"
+    "    var xhttp = new XMLHttpRequest();"
+    "    xhttp.open('POST', 'pulse_lights', true);"
+    "    xhttp.onload = function(){"
+    "      console.log(this.responseText);"
+    "      var inner;"
+    "      var outer;"
+    "      var style;"
+    "      var val;"
+    "      if(this.responseText.startsWith('on')) {"
+    "        inner = 'ON';"
+    "        style = 'color:Green;';"
+    "      } else {"
+    "        inner = 'OFF';"
+    "        style = 'color:Red;';"
+    "        val = 'Turn On';"
+    "      }"
+    "      if(this.responseText.endsWith('on')) {"
+    "        outer = 'because the main switch is on';"
+    "        document.getElementById('pulse_button').display = 'none';"
+    "      } else {"
+    "        outer = 'for ' + this.responseText.substring(this.responseText.indexOf(':') + 1);"
+    "        val = 'Restart Timer';"
+    "      }"
+    "      document.getElementById('pulse_button').value = val;"
+    "      document.getElementById('lights_span').innerHTML = inner;"
+    "      document.getElementById('lights_span').style = style;"
+    "      document.getElementById('outer_span').innerHTML = outer;"
+    "    };"
+    "    xhttp.send('poop');"
+    "  }"
+    "</script>";
 
   html += "<span style=\"font-size:90px\">";
 
   // Longest string example, 82 chars: Notifications are <span id='lights_span' style="color:Green;">ON</span>
   snprintf(httpStr, 82, "Lights are %s    ", lights_on ? "<span id='lights_span' style=\"color:Green;\">ON</span>" : "<span id='lights_span' style=\"color:Red;\">OFF</span>");
   html += httpStr;
-  html += "<input type='button' id='toggle_button' value='";
-  html += house_switch_on ? "disabled" : (lights_on ? "Turn Off" : "Turn On");
-  html += "' onclick='toggle()' style=\"font-size:90px; padding:20px 40px;\">";
+  html += "<span id='outer_span'>";
+  if (house_switch_on) {
+    html += "because the main switch is on";
+  } else if (lights_on) {
+    millisToDaysHoursMinutes( on_set_time - (millis() - on_start_time), time_left, 64);
+    html += "for ";
+    html += time_left;
+  }
+  
+  html += "</span>";
+  if (!house_switch_on) {
+    html += "<input type='button' id='pulse_button' value='";
+    html += lights_on ? "Restart Timer" : "Turn On";
+    html += "' onclick='pulse()' style=\"font-size:90px; padding:20px 40px;\">";
+  }
   html += "</span></br>";
   
   // Close it off
@@ -136,15 +227,18 @@ void init_remote_control()
     httpServer.sendHeader("Connection", "close");
     httpServer.send(200, "text/html", getSystemStatus());
   });
-  httpServer.on("/toggle_lights", HTTP_POST, []() {
-    String status = "";
+  httpServer.on("/pulse_lights", HTTP_POST, []() {
+    String status = "%s:%s";
     on_request = web_interface;
     handle_light_requests();
 
+    millisToDaysHoursMinutes( on_set_time - (millis() - on_start_time), time_left, 64);
+
     if(lights_on) status = "on";
     else status = "off";
-    if(house_switch_on) status += ":on";
-    else status += ":off";
+    status += ":";
+    if(house_switch_on) status += "on";
+    else status += time_left;
     status.toCharArray(responseStr, status.length() + 1);
     httpServer.send(200, "text/plain", responseStr);
   });
@@ -248,7 +342,7 @@ void setup() {
 
   if(connectToWifi()) init_remote_control();
 
-  on_set_time = 3000;
+  on_set_time = 60000;
   current_switch_state = last_switch_state = stable_state = digitalRead(HOUSE_SWITCH);
   last_change_time = millis();
 }
@@ -265,7 +359,7 @@ void loop() {
   
   if(house_switch_changed) {
     // start debounce
-    if (millis() - last_change_time > 200) {
+    if (millis() - last_change_time > 100) {
       if (last_switch_state != stable_state) {
         // Debounce period done. React to switch state.
         stable_state = last_switch_state;
@@ -289,8 +383,7 @@ void loop() {
   
   if (lights_on) {
     // How long have they been on?
-    on_time = millis() - on_start_time;
-    if (on_time >= on_set_time) {
+    if (millis() - on_start_time >= on_set_time) {
       // on time has expired. React.
       off_request = timer;
     }
